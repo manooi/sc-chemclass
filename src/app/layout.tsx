029ec4a730d12def5472c989b352a48cc6ff4470
sirawit.mahanin <sirawit.mn@gmail.com>
2024-12-29 18:27:11 +0700

    Change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,16 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Script from "next/script";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "../fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "../fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "SC Chemclass",
@@ -32,10 +32,17 @@ export default function RootLayout({
           src="https://unpkg.com/@ruffle-rs/ruffle"
           strategy="beforeInteractive"
         />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body
         // overflow-y-scroll fix problem with navbar
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-y-scroll`}
+        className={`antialiased overflow-y-scroll`}
       >
         {children}
       </body>
